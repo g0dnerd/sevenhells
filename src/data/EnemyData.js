5e27edc943ff6fcf8eee82db83743eeb36a03dd1
Paul Kukowski <kukowskipaul@gmail.com>
2023-11-29 16:11:00 +0100

    Refactored enemy data into separate Data classes in /src/data. Added a fancy new backdrop.

---
+++ src/data/EnemyData.js
@@ -0,0 +1,17 @@
+export default class EnemyData {
+
+ static get hpValues() {
+    return [
+        100, // Basic Human
+        80 // Consultant
+    ];
+ }
+
+ static get goldValues() {
+    return [
+        10, // Basic Human
+        20 // Consultant
+    ];
+ }
+
+}
